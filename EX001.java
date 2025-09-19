d2ff069959462a61be17d23b299cfba366690e0d
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-09-19 11:38:43 -0300

    Create EX001.java

---
+++ EX001.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class jonathan {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        int x;
+
+        System.out.print("x: ");
+        if (!sc.hasNextInt()){
+            System.out.print("Erro!");
+            return;
+        }
+        x = sc.nextInt();
+
+        if (x < 1 || x > 12){
+            System.out.print("Erro!");
+        }
+        else {
+            if (x == 1){
+                System.out.print("Janeiro");
+            } else if (x == 2) {
+                System.out.print("Fevereiro");
+            }
+            else if (x == 3) {
+                System.out.print("marco");
+            }
+            else if (x == 4) {
+                System.out.print("abril");
+            }
+            else if (x == 5) {
+                System.out.print("maio");
+            }
+            else if (x == 6) {
+                System.out.print("junho");
+            }
+            else if (x == 7) {
+                System.out.print("julho");
+            }else if (x == 8) {
+                System.out.print("agosto");
+            }else if (x == 9) {
+                System.out.print("setembro");
+            }else if (x == 10) {
+                System.out.print("outubro");
+            }
+            else if (x == 11) {
+                System.out.print("novembro");
+            }
+            else {
+                System.out.print("dezembro");
+            }
+        }
+    }
+
+}
